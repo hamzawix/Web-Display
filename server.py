da4f4e0bd8b915a44bc278ef6353762e0af5701b
Hamza Boughraira <hamza.boughraira@gmail.com>
2017-05-08 22:12:25 +0100

    Creating the server using the Flask framework

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import random
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+@app.route('/temp')
+def temp():
+	n = random.random() * 100
+	return str(n)
+
+@app.route('/carb')
+def carb():
+	k = random.random() * 100
+	return str(k)
+
+@app.route('/vit')
+def vit():
+	v = random.random() * 100
+	return str(v)
+
+
+if __name__ == '__main__':
+	app.run()
