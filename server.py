3b34f82329afa3d13992339efb5fa7f60b594a6d
Hamza Boughraira <hamza.boughraira@gmail.com>
2017-05-22 23:32:30 +0100

    csv timed data

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 #!/usr/bin/ python3.5
 from flask import Flask, render_template, jsonify
 import random
-app = Flask(__name__)
+import csv
+from datetime import datetime
 
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -12,15 +14,43 @@ def index():
 def charts():
 	return render_template("charts.html")
 
+@app.route('/timed_data')
+def timed_data():
+
+	mydict = {"temp":[], "carb":[], "vit":[]}
+	datafile = csv.reader(open(r"data.csv", "rt"))
+	for row in datafile:
+		mydict["temp"].append(row[0])
+		mydict["carb"].append(row[1])
+		mydict["vit"].append(row[2])
+
+	return jsonify(mydict)
+
 @app.route('/data')
 def data():
-	t = random.random() * 100
-	c = random.random() * 100
-	v = random.random() * 100
+	t = round(random.random() * 100, 2)
+	c = round(random.random() * 100, 2)
+	v = round(random.random() * 100, 2)
+
+	l = [t,c,v]
+
+	with open(r"data.csv", 'a') as f:
+		w = csv.writer(f)
+		w.writerow(l)
+
+	with open(r"time.csv", 'a') as f:
+		w = csv.writer(f)
+		w.writerow(["{}:{}:{}".format(datetime.now().hour, datetime.now().minute, datetime.now().second)])
 
 	return jsonify({'t': int(t),
 					'c': int(c),
 					'v': int(v)})
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=5000)
+
+	r = open(r"data.csv", "w+")
+	r.close()
+	
+	r = open(r"time.csv", "w+")
+	r.close()
+	app.run(host="localhost", port=5000)
--- static/scripts.js
+++ static/scripts.js
@@ -13,7 +13,6 @@ function getdata(){
 		var t = parseFloat(d.t).toFixed(2);
 		var c = parseFloat(d.c).toFixed(2);
 		var v = parseInt(d.v).toFixed(2);
-		console.log(t);
 		g.refresh(t);
 		k.refresh(c);
 		$("#speed").focus();
